112def0aea1f0fc88118a4d2d2770cc6fa8834fe
Nicolas Ribeiro <lunary@niskii.dev>
2022-09-25 20:55:22 -0300

    First commit

---
+++ Aula01/Aula01.cs
@@ -0,0 +1,15 @@
+﻿namespace Csharp_Course.Aula01
+{
+    class Aula01
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hi, how are you?");
+            var condition = Console.ReadLine();
+            
+            Console.WriteLine("Your condition is: " + condition);
+
+            Console.ReadKey();
+        }
+    }
+}
